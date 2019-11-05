ab792c60f815dda12115119498da5e4ea43c5793
Annaick Lewis Dagnino <annaick1995@gmail.com>
2019-11-05 22:44:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,24 +1,20 @@
 // write your code below!
 function happyHolidays(){
-  return("Happy holidays!")
+  return(`Happy holidays!`)
 }
 
 
 function happyHolidaysTo(name){
-  var text = 'Happy holidays, ${name}!'
-  return(text)
+  return(`Happy holidays, ${name}!`)
 }
 
-function sayHelloTo(firstName) {
-  console.log(`Hello, ${firstName}!`)
-}
 
 function happyHolidayTo(holiday, name){
-  return("Happy ${holiday}, ${name}!")
+  return(`Happy ${holiday}, ${name}!`)
 } 
 
 
 function holidayCountdown(holiday, numDays){
-  return('It\'s ${numDays} days until ${holiday}!' )
+  return(`It\'s ${numDays} days until ${holiday}!`)
 }
 
